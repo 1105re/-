79620f766696f1164b6955ed4d170e08df7cfa10
rie <rie011005@gmail.com>
2023-02-17 13:41:51 +0900

    021703rieyui

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        if(\App::enviroment(['production'])){
-            \URL::forceSchema('https');
+        if(\App::environment(['production'])){
+            \URL::forceScheme('https');
         }
         
     }
